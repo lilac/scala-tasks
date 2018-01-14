20dac882d9ffdb672988e6437b8f12f35338bb79
Junjun Deng <cnjamesdeng@gmail.com>
2018-01-14 15:00:38 +0800

    Clean code

--- load-test/src/test/scala/WsSimulation.scala
+++ load-test/src/test/scala/WsSimulation.scala
@@ -6,19 +6,10 @@ import io.gatling.http.Predef._
 class WsSimulation extends Simulation {
   val httpConf = http
     .baseURL("http://echo.websocket.org")
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Gatling2")
     .wsBaseURL("wss://echo.websocket.org")
 
   val scn = scenario("WebSocket")
-//    .exec(http("Home").get("/"))
-//    .pause(1)
-//    .exec(session => session.set("id", "Steph" + session.userId))
-//    .exec(http("Login").get("/room?username=${id}"))
-//    .pause(1)
     .exec(ws("Connect WS").open("/"))
     .pause(1)
     .repeat(2, "i") {
@@ -29,8 +20,9 @@ class WsSimulation extends Simulation {
     }
     .exec(ws("Close WS").close)
 
-  setUp(scn.inject(
-    constantUsersPerSec(10) during (1 seconds)
+  setUp(
+    scn.inject(
+      constantUsersPerSec(10) during (1 seconds)
     )
   ).protocols(httpConf)
 }
